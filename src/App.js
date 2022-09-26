68ea54b3f22d7e7a332922e5d909af685692157a
manitheleo <mkolli@amphora.net>
2022-09-26 14:15:11 +0530

    add container

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 
 import './App.css';
 import { Container } from '@mui/system';
-import { TextField, Typography } from '@mui/material';
-import { useState } from 'react';
+
+
 
 function App() {
-  const[data,setData]=useState('')
+ 
+ 
+
   return (
-    <>
-    <center>
-    <Typography variant='h2'>Todo</Typography>
-    <Container>
-      <TextField type="text" style={{"width":"500px","margin":"5px"}} label="Name" variant="outlined" onChange={(e)=>setData(e.target.value)}></TextField>
-    </Container>
-    {data}
-    </center>
-    </>
+    <Container>my first container</Container>
+    
       
   );
 }
