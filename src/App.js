2c78d23a3ebe5b9d644f99ab289163fb133ef96d
manitheleo <mkolli@amphora.net>
2022-09-29 14:35:43 +0530

    install eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,55 +1,54 @@
-
-import './App.css';
-import {TextField,Container,List, ListItem, ListItemIcon, ListItemText, Button, InputAdornment} from '@mui/material'
-import FolderIcon from '@mui/icons-material/Folder';
-
+import "./App.css";
+import {
+  TextField,
+  Container,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Button,
+  InputAdornment,
+} from "@mui/material";
+import FolderIcon from "@mui/icons-material/Folder";
 
 function App() {
- 
   return (
-   
-       
-       
-       <Container>
-         
-        
-        <TextField label="Name" variant="outlined" InputProps={{ endAdornment: (
+    <Container>
+      <TextField
+        labl="Name"
+        variant="outlined"
+        InputProps={{
+          endAdornment: (
             <InputAdornment position="end">
-              <Button variant='contained' color="primary">
-              add to todo list
+              <Button variant="contained" color="primary">
+                add to todo list
               </Button>
             </InputAdornment>
           ),
         }}
       />
-       <List dense>
-        
-                <ListItem >
-                    <ListItemIcon><FolderIcon /></ListItemIcon>
-                    <ListItemText primary="Single-line item"/>
-                </ListItem>
+      <List dense>
+        <ListItem>
+          <ListItemIcon>
+            <FolderIcon />
+          </ListItemIcon>
+          <ListItemText primary="Single-line item" />
+        </ListItem>
 
-                <ListItem>
-                  <ListItemIcon>
-                    <FolderIcon />
-                     </ListItemIcon>
-                  <ListItemText
-                    primary="Single-line item"/>
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <FolderIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Single-line item"/>
-                 
-                  
-                </ListItem>
-                </List>
-          
-               
-                </Container>
-               
-               
+        <ListItem>
+          <ListItemIcon>
+            <FolderIcon />
+          </ListItemIcon>
+          <ListItemText primary="Single-line item" />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <FolderIcon />
+          </ListItemIcon>
+          <ListItemText primary="Single-line item" />
+        </ListItem>
+      </List>
+    </Container>
   );
 }
 
