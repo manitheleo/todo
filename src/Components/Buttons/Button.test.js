1407f6c02b0644ec19979f6a34e1169a58472432
manitheleo <mkolli@amphora.net>
2022-10-28 13:18:11 +0530

    add delete testing

--- src/Components/Buttons/Button.test.js
+++ src/Components/Buttons/Button.test.js
@@ -1,5 +1,6 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import DeleteButton from "./DeleteButton";
 import EditButton from "./EditButton";
 
 test("Should return todo item current text, Given the user click on edit button", async () => {
@@ -13,4 +14,16 @@ test("Should return todo item current text, Given the user click on edit button"
   );
 
   expect(handleEditClick).toHaveBeenCalledWith("first todo item");
+});
+
+test("should delete todo item current text,Given the user click delete button", async () => {
+  let id = "1";
+  const handleDeleteItem = jest.fn();
+  render(<DeleteButton handleDeleteItem={handleDeleteItem} id={id} />);
+  userEvent.click(
+    screen.getByRole("button", {
+      name: /delete/i,
+    })
+  );
+  expect(handleDeleteItem).toHaveBeenCalledWith("1");
 });
