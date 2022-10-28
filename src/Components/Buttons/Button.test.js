ee64289f9d1c13ddd1641354e9a427ae138d8f79
manitheleo <mkolli@amphora.net>
2022-10-28 15:22:53 +0530

    replace let to const variable

--- src/Components/Buttons/Button.test.js
+++ src/Components/Buttons/Button.test.js
@@ -1,10 +1,10 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import DeleteButton from "./DeleteButton";
 import EditButton from "./EditButton";
 
 test("Should return todo item current text, Given the user click on edit button", async () => {
-  let item = "first todo item";
+  const item = "first todo item";
   const handleEditClick = jest.fn();
   render(<EditButton handleEditClick={handleEditClick} item={item} />);
   userEvent.click(
@@ -17,7 +17,7 @@ test("Should return todo item current text, Given the user click on edit button"
 });
 
 test("should delete todo item current text,Given the user click delete button", async () => {
-  let id = "1";
+  const id = "1";
   const handleDeleteItem = jest.fn();
   render(<DeleteButton handleDeleteItem={handleDeleteItem} id={id} />);
   userEvent.click(
