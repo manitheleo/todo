e51ea17c81535b9f4e3782d11b14304a386038af
manitheleo <mkolli@amphora.net>
2022-10-31 16:21:42 +0530

    changes updated

--- src/Components/Forms/forms.test.js
+++ src/Components/Forms/forms.test.js
@@ -2,19 +2,19 @@ import { screen, render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import EditForm from "./EditForm";
 
-test("when the user click edit form", async () => {
+test("when the user click edit form", () => {
   const handleEditFormSubmit = jest.fn();
   render(
-    <EditForm onSubmit={handleEditFormSubmit} currentTodo={"first todo item"} />
+    <EditForm onSubmit={handleEditFormSubmit} currentTodo="first todo item" />
   );
-
-  userEvent.type(screen.getByRole("textbox"));
+  const textInput = screen.getByRole("textbox");
+  userEvent.type(textInput, "first todo item");
   userEvent.click(
     screen.getByRole("button", {
       name: /update/i,
     })
   );
-  waitFor(() =>
-    expect(handleEditFormSubmit).toHaveBeenCalledWith("first todo item")
-  );
+  waitFor(() => {
+    expect(handleEditFormSubmit).toHaveBeenCalledWith("first todo item");
+  });
 });
